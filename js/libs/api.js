32a65bef63950d45d3db74775cdc81c9d61beea6
Carrie Wladis <carriewladis@gmail.com>
2014-05-03 12:43:51 -0400

    First Commit

---
+++ js/libs/api.js
@@ -0,0 +1,93 @@
+var imageData = {};
+    // imageData.contentSrc;
+function getImages(){
+	$.ajax({
+		method: "GET",
+		dataType: "json",
+		url: "http://www.brooklynmuseum.org/opencollection/api/oai.pmh.php?api_key=Cd3V0ow1u6",
+		
+		headers: { "Content-Type": "application/json"},//this is a security thing and SHOULD be looked up later; is an object as well which you can see from the {}
+		success: function(data){
+			imageData = data;
+            buildGallery();		},
+		error: function(request, error, response){ //this is for when things go wrong as they will since we don't have access to the server today
+			assignBackUp();
+		}
+	})
+};
+
+function assignBackUp(){
+	imageData = [
+    {
+              "uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size2\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_001_PS4.jpg",
+              "thumb_uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size0\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_001_PS4.jpg",
+              "credit": "Brooklyn Museum photograph",
+              "description": "The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013. [06\/28\/2013 - 09\/22\/2013]. Installation view.",
+              "caption": " The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013, June 28, 2013 through September 22, 2013",
+            },
+            {
+              "uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size2\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_010_PS4.jpg",
+              "thumb_uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size0\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_010_PS4.jpg",
+              "credit": "Brooklyn Museum photograph",
+              "description": "The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013. [06\/28\/2013 - 09\/22\/2013]. Installation view.",
+              "caption": " The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013, June 28, 2013 through September 22, 2013",
+            },
+            {
+              "uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size2\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_009_PS4.jpg",
+              "thumb_uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size0\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_009_PS4.jpg",
+              "credit": "Brooklyn Museum photograph",
+              "description": "The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013. [06\/28\/2013 - 09\/22\/2013]. Installation view.",
+              "caption": " The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013, June 28, 2013 through September 22, 2013",
+            },
+            {
+              "uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size2\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_008_PS4.jpg",
+              "thumb_uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size0\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_008_PS4.jpg",
+              "credit": "Brooklyn Museum photograph",
+              "description": "The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013. [06\/28\/2013 - 09\/22\/2013]. Installation view.",
+              "caption": " The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013, June 28, 2013 through September 22, 2013",
+            },
+            {
+              "uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size2\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_007_PS4.jpg",
+              "thumb_uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size0\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_007_PS4.jpg",
+              "credit": "Brooklyn Museum photograph",
+              "description": "The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013. [06\/28\/2013 - 09\/22\/2013]. Installation view.",
+              "caption": " The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013, June 28, 2013 through September 22, 2013 ",
+            },
+            {
+              "uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size2\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_006_PS4.jpg",
+              "thumb_uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size0\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_006_PS4.jpg",
+              "credit": "Brooklyn Museum photograph",
+              "description": "The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013. [06\/28\/2013 - 09\/22\/2013]. Installation view.",
+           	  "caption": " The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013, June 28, 2013 through September 22, 2013",
+            },
+            {
+              "uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size2\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_005_PS4.jpg",
+              "thumb_uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size0\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_005_PS4.jpg",
+              "credit": "Brooklyn Museum photograph",
+              "description": "The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013. [06\/28\/2013 - 09\/22\/2013]. Installation view.",
+              "caption": " The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013, June 28, 2013 through September 22, 2013",
+            },
+            {
+              "uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size2\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_004_PS4.jpg",
+              "thumb_uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size0\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_004_PS4.jpg",
+              "credit": "Brooklyn Museum photograph",
+              "description": "The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013. [06\/28\/2013 - 09\/22\/2013]. Installation view.",
+              "caption": " The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013, June 28, 2013 through September 22, 2013",
+            },
+            {
+              "uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size2\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_003_PS4.jpg",
+              "thumb_uri": "http:\/\/cdn2.brooklynmuseum.org\/images\/opencollection\/exhibitions\/size0\/DIG_E_2013_Bruce_High_Quality_Foundation_Ode_to_Joy_003_PS4.jpg",
+              "credit": "Brooklyn Museum photograph",
+              "description": "The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013. [06\/28\/2013 - 09\/22\/2013]. Installation view.",
+              "caption": " The Bruce High Quality Foundation: Ode to Joy, 2001\u20132013, June 28, 2013 through September 22, 2013",
+            },
+    ]; //this closes the array of data
+    buildGallery();
+}
+
+function buildGallery(){ //this builds all the images
+    // for(var x=0; x<imageData.length; x++)
+        $('#imgTry').attr('src', imageData[0].uri);
+};
+
+getImages(); 
---
+++ js/main.js
@@ -0,0 +1,93 @@
+var name,
+	title,
+	year,
+	medium,
+	size;
+
+$(document).ready(function(){
+
+	$(window).scroll(function(e){
+    	parallax();
+	});
+	
+	$('#contactEmail').submit(function(event){
+		event.preventDefault();
+		reset($(this));
+	});
+
+	$('#captionBtn').on('click', function(){ 
+			name = getName();
+			title = getTitle();
+			year = getYear();
+			medium = getMedium();
+			size = getSize();
+			var caption = name + ", '" + title +  "', " + year + ", " + medium + ", " + size;		 
+
+			writeCaption(caption); 
+			reset($(this));
+	}); //on click
+
+	function parallax(){
+    	var scrolled = $(window).scrollTop();
+        console.log(scrolled);
+        //$('#bgContainer').css('top', -(scrolled * .2) + 'px');
+}
+
+	
+
+	$('#contact').on('click', function(){
+		$('#mainText').slideUp("slow");
+		$('.slide').slideUp("slow");
+		$('#bgPost').slideUp("slow");	
+	}); //.on for slide
+
+	
+
+	
+$('form').mouseover(function(){
+		$('h2').css("color", "#f7f064");
+		$('#postEmail').addClass("show");
+	});
+
+	$('form').mouseout(function(){
+		$('h2').css("color", "#1b2331");
+	});
+	
+}); 
+
+function getName(){
+  			return $('#artistName').val(); 
+		};
+
+function getTitle(){
+  			return $('#title').val(); 
+		};
+
+function getYear(){
+  			return $('#year').val(); 
+		};
+
+function getMedium(){
+  			return $('#medium').val(); 
+		};
+
+function getSize(){
+  			return $('#size').val(); 
+		};
+
+function writeCaption(caption){
+		$('#oldCaption').hide();
+		$('#messageArea').html("<p>" + caption + "</p>");
+		};
+
+function reset($form){  //$form here could be any word like pony, but it references the form tag!
+	$form.find('input, textarea').val(''); //this puts the value of input fields to an empty string or ''
+};
+
+//function carouselImg(){
+	//this will give the user a choice of which img they want to change to on the Try It page
+// };
+
+
+
+
